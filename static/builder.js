35acb52e8bf4a836451866f6f0818fdf66778a07
Manitej Boorgu <68567672+techpixel@users.noreply.github.com>
2022-04-26 22:59:33 +0000

    updated

--- static/builder.js
+++ static/builder.js
@@ -2,7 +2,7 @@ var markdownUpdate = document.getElementById('markdown');
 var outputHTML = document.getElementById('content');
 
 function update() {
-  newHtml = sanitizeHtml(marked(markdownUpdate.value, { breaks: true }), {
+  newHtml = sanitizeHtml(marked.parse(markdownUpdate.value, { breaks: true }), {
   allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img', 'h1', 'h2' ])
   });
   outputHTML.innerHTML = newHtml;
